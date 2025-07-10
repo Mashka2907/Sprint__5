0d4e8fc3350bea6aa3e1d31ab667deda243933e3
Mashka <krymzova.m@mail.ru>
2025-07-10 18:32:06 +0300

    Добавить финальный проект

---
+++ src/data.py
@@ -0,0 +1,9 @@
+
+class UserData:
+    correct_email = "parfenova@gmail.com"
+    password = "qwerty"
+    not_correct_email = "invalid-email"
+    name_product = "Преступление и наказание"
+    description = "В хорошем состоянии. Эксклюзивная классика."
+    price = 350
+
